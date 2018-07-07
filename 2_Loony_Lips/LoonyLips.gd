3bd19d33aa949ab87a0c6ac3b48de67b39e2cda1
Derek Stobbs <dstobbs@googlemail.com>
2018-07-07 15:33:45 +0100

    2_Loony_Lips: Blackboard
    - Text is now output in a simple display, rather than to the console

--- 2_Loony_Lips/LoonyLips.gd
+++ 2_Loony_Lips/LoonyLips.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _ready():
 	var prompt = ["Derek", "cheeseburger", "wedges", "happy"]
-	var story = "Once apon a time, %s ate a %s with %s and was %s"
-	print (story % prompt)
+	var story = "Once apon a time, %s ate a %s with %s and was %s."
+	$Blackboard/StoryText.bbcode_text = story % prompt
