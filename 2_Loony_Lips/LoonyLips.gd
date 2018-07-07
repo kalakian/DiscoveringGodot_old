cccf3835eb1c0f96dad786c07a3633bdce2dfeca
Derek Stobbs <dstobbs@googlemail.com>
2018-07-07 15:58:28 +0100

    2_Loony_Lips: Input Text
    - User can now input text to replace the story text

--- 2_Loony_Lips/LoonyLips.gd
+++ 2_Loony_Lips/LoonyLips.gd
@@ -3,4 +3,13 @@ extends Node2D
 func _ready():
 	var prompt = ["Derek", "cheeseburger", "wedges", "happy"]
 	var story = "Once apon a time, %s ate a %s with %s and was %s."
-	$Blackboard/StoryText.bbcode_text = story % prompt
+	$Blackboard/StoryText.text = story % prompt
+	$Blackboard/TextBox.clear()
+
+func _on_OKButton_pressed():
+	var new_text = $Blackboard/TextBox.text
+	_on_TextBox_text_entered(new_text)
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
+	$Blackboard/TextBox.clear()
