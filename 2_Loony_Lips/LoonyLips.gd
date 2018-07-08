ab079884ada1eedfe3c2ccd7091eb8954fe403a0
Derek Stobbs <dstobbs@googlemail.com>
2018-07-08 16:21:46 +0100

    2_Loony_Lips: JSON
    - All stories and other text now loaded from JSON files

--- 2_Loony_Lips/LoonyLips.gd
+++ 2_Loony_Lips/LoonyLips.gd
@@ -1,25 +1,32 @@
 extends Node2D
 
-var intro_text = "Welcome to Loony Lips, a fun word game.\n\n"
 var player_words = []
-var template = [
-		{
-		"prompts" : ["a name", "a thing", "another thing", "a feeling"],
-		"story" : "Once apon a time, %s ate a %s with %s and was %s."
-		},
-		{
-		"prompts" : ["a place", "a thing", "a compass direction"],
-		"story" : "You awake in %s, there is %s here. There is an exit to the %s"
-		}
-		]
 var current_story
+var other_text
 
 func _ready():
 	randomize()
-	current_story = template[randi() % template.size()]
+	other_text = get_from_json("other-strings.json")
+	set_random_story()
+	$Blackboard/StoryText.text = other_text.intro_text
 	$Blackboard/TextBox.clear()
-	$Blackboard/StoryText.text = intro_text
 	prompt_player(false)
+
+
+func set_random_story():
+	var stories = get_from_json("stories.json")
+	current_story = stories[randi() % stories.size()]
+
+
+func get_from_json(filename):
+	var file = File.new()
+	file.open(filename, File.READ) # assumes the file exists
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	
+	return data
+
 
 func _on_OKButton_pressed():
 	if is_story_done():
@@ -28,19 +35,23 @@ func _on_OKButton_pressed():
 		var new_text = $Blackboard/TextBox.text
 		_on_TextBox_text_entered(new_text)
 
+
 func _on_TextBox_text_entered(new_text):
 	player_words.append(new_text)
 	$Blackboard/TextBox.clear()
 	check_player_word_length()
 
+
 func is_story_done():
-	return player_words.size() == current_story.prompts.size()
+	return player_words.size() == current_story.prompt.size()
+
 
 func prompt_player(clear = true):
 	if clear:
 		$Blackboard/StoryText.clear()
-	$Blackboard/StoryText.text += "Can I have %s, please?" % current_story.prompts[player_words.size()]
+	$Blackboard/StoryText.text += other_text.prompt % current_story.prompt[player_words.size()]
 	$Blackboard/TextBox.grab_focus()
+
 
 func check_player_word_length():
 	if is_story_done():
@@ -48,10 +59,12 @@ func check_player_word_length():
 	else:
 		prompt_player()
 
+
 func tell_story():
 	$Blackboard/StoryText.text = current_story.story % player_words
 	end_game()
 
+
 func end_game():
 	$Blackboard/TextBox.queue_free()
-	$Blackboard/OKButton/ButtonLabel.text = "Again!"
+	$Blackboard/OKButton/ButtonLabel.text = other_text.again
