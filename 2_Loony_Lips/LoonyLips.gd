4405747314d9ec67e731a11c40c3f6b9947e1db0
Derek Stobbs <dstobbs@googlemail.com>
2018-07-08 11:35:59 +0100

    2_Loony_Lips: Prompts
    - Intro text now shown on first starting
    - Player given prompts to enter specific things

--- 2_Loony_Lips/LoonyLips.gd
+++ 2_Loony_Lips/LoonyLips.gd
@@ -1,15 +1,34 @@
 extends Node2D
 
+var intro_text = "Welcome to Loony Lips, a fun word game.\n\n"
+var player_words = []
+var prompts = ["a name", "a thing", "another thing", "a feeling"]
+var story = "Once apon a time, %s ate a %s with %s and was %s."
+
 func _ready():
-	var prompt = ["Derek", "cheeseburger", "wedges", "happy"]
-	var story = "Once apon a time, %s ate a %s with %s and was %s."
-	$Blackboard/StoryText.text = story % prompt
 	$Blackboard/TextBox.clear()
+	$Blackboard/StoryText.text = intro_text
+	prompt_player(false)
 
 func _on_OKButton_pressed():
 	var new_text = $Blackboard/TextBox.text
 	_on_TextBox_text_entered(new_text)
 
 func _on_TextBox_text_entered(new_text):
-	$Blackboard/StoryText.text = new_text
+	player_words.append(new_text)
 	$Blackboard/TextBox.clear()
+	check_player_word_length()
+
+func prompt_player(clear = true):
+	if clear:
+		$Blackboard/StoryText.clear()
+	$Blackboard/StoryText.text += "Can I have %s, please?" % prompts[player_words.size()]
+
+func check_player_word_length():
+	if player_words.size() == prompts.size():
+		tell_story()
+	else:
+		prompt_player()
+
+func tell_story():
+	$Blackboard/StoryText.text = story % player_words
