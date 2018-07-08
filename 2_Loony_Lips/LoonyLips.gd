1c7d5e9ea126dc515df6f3bba3c4fe77250c3405
Derek Stobbs <dstobbs@googlemail.com>
2018-07-08 11:53:20 +0100

    2_Loony_Lips: End Game
    - Game now concluded when the story is told, and can be restarted

--- 2_Loony_Lips/LoonyLips.gd
+++ 2_Loony_Lips/LoonyLips.gd
@@ -7,17 +7,24 @@ var story = "Once apon a time, %s ate a %s with %s and was %s."
 
 func _ready():
 	$Blackboard/TextBox.clear()
+	$Blackboard/TextBox.grab_focus()
 	$Blackboard/StoryText.text = intro_text
 	prompt_player(false)
 
 func _on_OKButton_pressed():
-	var new_text = $Blackboard/TextBox.text
-	_on_TextBox_text_entered(new_text)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		var new_text = $Blackboard/TextBox.text
+		_on_TextBox_text_entered(new_text)
 
 func _on_TextBox_text_entered(new_text):
 	player_words.append(new_text)
 	$Blackboard/TextBox.clear()
 	check_player_word_length()
+
+func is_story_done():
+	return player_words.size() == prompts.size()
 
 func prompt_player(clear = true):
 	if clear:
@@ -25,10 +32,15 @@ func prompt_player(clear = true):
 	$Blackboard/StoryText.text += "Can I have %s, please?" % prompts[player_words.size()]
 
 func check_player_word_length():
-	if player_words.size() == prompts.size():
+	if is_story_done():
 		tell_story()
 	else:
 		prompt_player()
 
 func tell_story():
 	$Blackboard/StoryText.text = story % player_words
+	end_game()
+
+func end_game():
+	$Blackboard/TextBox.queue_free()
+	$Blackboard/OKButton/ButtonLabel.text = "Again!"
