74a2207c427ebdf816b7ec7449fc72beb6a8e3fe
Derek Stobbs <dstobbs@googlemail.com>
2018-07-09 12:05:36 +0100

    3_Hoppy_Days: Animated Bunny
    - The Bunny (Player) is now animated while idle or moving

--- 3_Hoppy_Days/Scripts/Player.gd
+++ 3_Hoppy_Days/Scripts/Player.gd
@@ -11,5 +11,14 @@ func _physics_process(delta):
 		motion.x += SPEED
 	if Input.is_action_pressed("left"):
 		motion.x -= SPEED
-	
 	move_and_slide(motion)
+	
+	if motion.x > 0:
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+	elif motion.x < 0:
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
+		$AnimatedSprite.flip_h = false
