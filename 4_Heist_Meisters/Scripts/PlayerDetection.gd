62b800bdb9d4a653aa9100f4a5346ef208014def
Derek Stobbs <dstobbs@googlemail.com>
2018-12-10 12:03:07 +0000

    4_Heist_Meisters: LOS Detection
    - Security camera now checks to see if the player is in line of sight, and within its max distance

--- 4_Heist_Meisters/Scripts/PlayerDetection.gd
+++ 4_Heist_Meisters/Scripts/PlayerDetection.gd
@@ -1,13 +1,14 @@
 extends "res://Scripts/Character.gd"
 
 const FOV_TOLERANCE = deg2rad(18)
+const MAX_DETECTION_RANGE = 320
 const RED = Color(1, .25, .25)
 const WHITE = Color(1, 1, 1)
 
 onready var Player = get_node("/root/Level1/Player") # make this level neutral
 
 func _process(delta):
-	if Player_is_in_FOV_TOLERANCE():
+	if Player_is_in_FOV_TOLERANCE() and PLayer_is_in_LOS():
 		$Torch.color = RED
 	else:
 		$Torch.color = WHITE
@@ -20,4 +21,15 @@ func Player_is_in_FOV_TOLERANCE():
 	if abs(direction_to_Player.angle_to(NPC_facing_direction)) < FOV_TOLERANCE:
 		return true
 	return false
+
+
+func PLayer_is_in_LOS():
+	var space = get_world_2d().direct_space_state
+	var LOS_obstacle = space.intersect_ray(global_position, Player.global_position, [self], collision_mask)
 	
+	var distance_to_player = Player.global_position.distance_to(global_position)
+	var Player_in_range = distance_to_player <= MAX_DETECTION_RANGE
+	
+	if LOS_obstacle.collider == Player and Player_in_range:
+		return true
+	return false
